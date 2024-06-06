aa91d136478ddc0a69f88b1db201d4b541d27a39
alescrnjar <a.crnjar@gmail.com>
2024-06-06 17:27:29 -0400

    .

--- Training and Sampling/exec_train_AC.sh
+++ Training and Sampling/exec_train_AC.sh
@@ -8,7 +8,16 @@ echo "conda activate d3"
 # conda remove --name d3 --all
 
 #cd "../../../D3-DNA-Discrete-Diffusion/Training and Sampling"
-python train.py noise.type=geometric graph.type=uniform model=small model.scale_by_sigma=False
+#python train.py noise.type=geometric graph.type=uniform model=small model.scale_by_sigma=False
+
+visdevs=$1 #0,1,2,3
+if [ ${#visdevs} = 0 ] ; then visdevs=0 ; fi
+echo "Visible devices: $visdevs"
+date
+CUDA_LAUNCH_BLOCKING=1 CUDA_VISIBLE_DEVICES=$visdevs python train.py noise.type=geometric graph.type=uniform model=small model.scale_by_sigma=False
+
+
+
 
 # PROSS: esegui senza il .h5 di deepstarr: cosi' capirai dove manca. Probabilmente data.py get_dataloaders
 
