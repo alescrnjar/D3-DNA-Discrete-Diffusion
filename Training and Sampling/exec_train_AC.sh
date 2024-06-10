63601a0d6f672488bb608f09a518d10ee5d55416
alescrnjar <a.crnjar@gmail.com>
2024-06-10 10:59:31 -0400

    .

--- Training and Sampling/exec_train_AC.sh
+++ Training and Sampling/exec_train_AC.sh
@@ -1,25 +1,92 @@
 #!/bin/bash
 
-#direc="../../../D3-DNA-Discrete-Diffusion/Training\ and\ Sampling"
-#direc='../../../D3-DNA-Discrete-Diffusion/Training and Sampling'
-#cd $direc
+batch_size=128 #128
+name=lentimpra #deepstarr
 
-echo "conda activate d3"
-# conda remove --name d3 --all
+echo \
+'
+defaults:
+  - _self_
+  - model: small
+  - override hydra/launcher: submitit_slurm
 
-#cd "../../../D3-DNA-Discrete-Diffusion/Training and Sampling"
-#python train.py noise.type=geometric graph.type=uniform model=small model.scale_by_sigma=False
+ngpus: 1
+tokens: 4
 
+training:
+  batch_size: '$batch_size'
+  accum: 1
+  n_iters: 500000
+  snapshot_freq: 50000
+  log_freq: 5000
+  eval_freq: 5000
+  snapshot_freq_for_preemption: 10000
+  weight: standard
+  snapshot_sampling: True
+  ema: 0.9999
+
+data:
+  train: '$name'
+  valid: '$name'
+  cache_dir: data
+
+graph:
+  type: absorb
+  file: data
+  report_all: False
+
+noise:
+  type: loglinear
+  sigma_min: 1e-4
+  sigma_max: 20
+
+sampling:
+  predictor: euler
+  steps: 128
+  noise_removal: True
+
+eval:
+  batch_size: 256
+  perplexity: False
+  perplexity_batch_size: 32
+
+optim:
+  weight_decay: 0
+  optimizer: AdamW
+  lr: 3e-4
+  beta1: 0.9
+  beta2: 0.999
+  eps: 1e-8
+  warmup: 2500
+  grad_clip: 1.
+
+
+hydra:
+  run:
+    dir: exp_local/${data.train}/${now:%Y.%m.%d}/${now:%H%M%S}
+  sweep:
+    dir: exp/${data.train}/${now:%Y.%m.%d}/${now:%H%M%S}
+    subdir: ${hydra.job.num}
+  launcher:
+    max_num_timeout: 100000
+    # timeout_min: 10079
+    partition: g40x
+    account: stanford
+    mem_gb: 96
+    cpus_per_task: 40
+    gpus_per_node: ${ngpus}
+    constraint: null
+
+' > ./Training\ and\ Sampling/configs/config/config.yaml
+
+
+
+#echo "conda activate d3"
 visdevs=$1 #0,1,2,3
 if [ ${#visdevs} = 0 ] ; then visdevs=0 ; fi
 echo "Visible devices: $visdevs"
 date
 CUDA_LAUNCH_BLOCKING=1 CUDA_VISIBLE_DEVICES=$visdevs python train.py noise.type=geometric graph.type=uniform model=small model.scale_by_sigma=False
-
-
-
-
-# PROSS: esegui senza il .h5 di deepstarr: cosi' capirai dove manca. Probabilmente data.py get_dataloaders
 
 #pip install hydra can work but if it doesnt:
 #pip install hydra-core==1.3.2
