e313153b17d5ff5dea426d8f546233236979f66b
alescrnjar <a.crnjar@gmail.com>
2024-07-03 16:46:25 -0400

    .

---
+++ Training_and_Sampling_Conditioned/model/__init__.py
@@ -0,0 +1,2 @@
+#from .transformer import SEDD
+from .transformer_mpra import SEDD #AC
---
+++ Training_and_Sampling_Conditioned/run_sample_XXX.py
@@ -0,0 +1,75 @@
+import torch
+import argparse
+import sys
+import numpy as np #AC
+
+import data
+from load_model import load_model_local
+import torch.nn.functional as F
+import sampling
+import h5py, os
+from torch.utils.data import DataLoader, TensorDataset, DistributedSampler
+
+#from PL_DeepSTARR import *
+import sys #AC
+sys.path.append('../../Occasio_Dev/src/') #AC
+from PL_Models import *
+
+def main():
+    parser = argparse.ArgumentParser(description="Generate some samples")
+    parser.add_argument("--model_path", default="", type=str) #Need to provide model path
+    parser.add_argument("--batch_size", type=int, default=256)
+    parser.add_argument("--steps", type=int, default=249) #Sequence length
+    args = parser.parse_args()
+
+    
+    device = torch.device('cuda')
+    model, graph, noise = load_model_local(args.model_path, device)
+
+    #Deepstarr
+    #filepath = os.path.join('DeepSTARR_data.h5') #load DeepSTARR data
+    #filepath = os.path.join('../../Occasio_Dev/inputs/DeepSTARR_data.h5') #load DeepSTARR data
+    #filepath = os.path.join('../../Occasio_Dev/inputs/DeepSTARRdev.h5') 
+    #filepath = os.path.join('../../Occasio_Dev/inputs/LentiMPRA_processed_for_dal.h5') #AC
+    #filepath = os.path.join('../../Occasio_Dev/inputs/newLentiMPRAK562_processed_for_dal.h5') #AC
+    #filepath = os.path.join('../../Occasio_Dev/inputs/newLentiMPRAHepG2_processed_for_dal.h5') #AC
+    filepath = os.path.join('../../Occasio_Dev/inputs/XXX.h5') #AC
+    data = h5py.File(filepath, 'r')
+    #ckpt_aug_path = os.path.join('oracle_DeepSTARR_DeepSTARR_data.ckpt') #Load DeepSTARR oracle model
+    #ckpt_aug_path = os.path.join('../Occasio_Dev/inputs/oracle_ResidualBind_LentiMPRA_processed_for_dal_relustandard1.ckpt') #AC
+    #ckpt_aug_path = os.path.join('../../Occasio_Dev/inputs/oracle_LegNet_LentiMPRA_processed_for_dal_bs100.ckpt') #AC
+    #ckpt_aug_path = os.path.join('../../Occasio_Dev/inputs/oracle_NewResNet_newLentiMPRAK562_processed_for_dal_finetune.ckpt') #AC
+    ckpt_aug_path = os.path.join('../../Occasio_Dev/inputs/oracle_YYY_XXX_finetune.ckpt') #AC
+    #ckpt_aug_path = os.path.join('../../Occasio_Dev/inputs/oracle_NewResNet_newLentiMPRAHepG2_processed_for_dal_finetune.ckpt') #AC
+
+    #We select test data to calculate MSE and generate samples. Change if required
+    X_test = torch.tensor(np.array(data['X_test']))
+    y_test = torch.tensor(np.array(data['Y_test']))
+    X_test = torch.argmax(X_test, dim=1)
+    testing_ds = TensorDataset(X_test, y_test)
+    test_ds = torch.utils.data.DataLoader(testing_ds, batch_size=args.batch_size, shuffle=False,
+                                             num_workers=4)
+    #deepstarr = PL_DeepSTARR.load_from_checkpoint(ckpt_aug_path, input_h5_file=filepath).eval()
+    #deepstarr = PL_LegNet.load_from_checkpoint(ckpt_aug_path, input_h5_file=filepath).eval() #AC
+    deepstarr = PL_LegNetPK.load_from_checkpoint(ckpt_aug_path, input_h5_file=filepath).eval() #AC  #V/CONDITIONING
+
+    val_pred_seq = []
+    for _, (batch, val_target) in enumerate(test_ds):
+        sampling_fn = sampling.get_pc_sampler(
+            graph, noise, (batch.shape[0], LLL), 'analytic', args.steps, device=device
+        )
+        #sample = sampling_fn(model) #VANILLA/C
+        sample = sampling_fn(model, val_target.to(device)) #V/CONDITIONING
+        seq_pred_one_hot = torch.nn.functional.one_hot(sample, num_classes=4).float()
+        val_pred_seq.append(seq_pred_one_hot)
+
+    val_pred_seqs = torch.cat(val_pred_seq, dim=0)
+    val_score = deepstarr.predict_custom(deepstarr.X_test.to(device))  #V/CONDITIONING
+    val_pred_score = deepstarr.predict_custom(val_pred_seqs.permute(0, 2, 1).to(device))  #V/CONDITIONING
+    sp_mse = (val_score - val_pred_score) ** 2  #V/CONDITIONING
+    mean_sp_mse = torch.mean(sp_mse).cpu()  #V/CONDITIONING
+    print(f"all-sp-mse {mean_sp_mse}")  #V/CONDITIONING
+    np.savez(os.path.join(args.model_path, f"sample_{rank}.npz", ), val_pred_seqs.cpu())  #V/CONDITIONING
+
+if __name__=="__main__":
+    main()
